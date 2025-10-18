4bf51ea54fd80a5a7db65a0455c925ac02d25169
criscandeloro <criscandeloro@gmail.com>
2025-10-18 10:34:54 -0300

    Pipeline

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  retries: {
+    runMode: 0,
+    openMode: 2,
+  },
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
