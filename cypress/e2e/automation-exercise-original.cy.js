3c27d531a9eecf918391b7b612783241e34ff26a
criscandeloro <criscandeloro@gmail.com>
2025-10-18 10:02:49 -0300

    Pipeline

--- cypress/e2e/automation-exercise-original.cy.js
+++ cypress/e2e/automation-exercise-original.cy.js
@@ -1,153 +1,111 @@
-
-
 /// <reference types="cypress" />
 
-import userData from "../fixtures/example.json"
-const { log } = require("console");
-
+import userData from "../fixtures/example.json";
 
 describe('Automation Exercise', () => {
-  beforeEach(() => {
+  const user = {};
 
-   cy.viewport('iphone-xr')
-      
-    //abrir o site
-   cy.visit('https://automationexercise.com/')
-
-    //Clicar no botão para criar um novo usuário
-   cy.get('a[href="/login"]').click()
-
-  })
-
- 
-  it('Cadastrar um usuário', () => {
-
+  before(() => {
     const timeStamp = new Date().getTime();
-  
-    //abre o link para informar os dados do novo usuário
-     cy.get('[data-qa="signup-name"]').type('QA tester')
-     cy.get('[data-qa="signup-email"]').type( `qa-tester-${timeStamp}@teste.com `)
-    
-   // cy.get('[data-qa="signup-button"]').click()
-   cy.contains('button','Signup').click()
+    user.name = 'QA tester';
+    user.email = `qa-tester-${timeStamp}@teste.com`;
+    user.password = '12345';
+  });
 
-   //Tela de cadastro
+  beforeEach(() => {
+    cy.viewport('iphone-xr');
+    cy.visit('https://automationexercise.com/');
+  });
 
-   //cy.get('#id_gender1').check()
-   cy.get('input[type=radio]').check('Mrs')
-   //cy.get('input[type=password]').type('12345')
-   cy.get('input#password').type('12345', {log:false})
+  it('Cadastrar um usuário', () => {
+    cy.get('a[href="/login"]').click();
+    cy.get('[data-qa="signup-name"]').type(user.name);
+    cy.get('[data-qa="signup-email"]').type(user.email);
+    cy.contains('button', 'Signup').click();
 
-   //para combobox e selectors
-   cy.get('[data-qa=days]').select('20')
-   cy.get('[data-qa=months]').select('September')
-   cy.get('[data-qa=years]').select('1992')
+    // Tela de cadastro
+    cy.get('input[type=radio]').check('Mrs');
+    cy.get('input#password').type(user.password, { log: false });
+    cy.get('[data-qa=days]').select('20');
+    cy.get('[data-qa=months]').select('September');
+    cy.get('[data-qa=years]').select('1992');
+    cy.get('input[type=checkbox]#newsletter').check();
+    cy.get('input[type=checkbox]#optin').check();
+    cy.get('input#first_name').type('Bob');
+    cy.get('input#last_name').type('Narciso Pipoca');
+    cy.get('input#company').type('PGATS');
+    cy.get('input#address1').type('Avenida Selenium, nr 2004');
+    cy.get('select#country').select('Canada');
+    cy.get('input#state').type('California');
+    cy.get('input#city').type('Los Angeles');
+    cy.get('[data-qa="zipcode"]').type('901001');
+    cy.get('[data-qa="mobile_number"]').type('111 222 333');
+    cy.get('[data-qa="create-account"]').click();
 
-   //radio button e checkboxs
-   cy.get('input[type=checkbox]#newsletter').check()
-   cy.get('input[type=checkbox]#optin').check()
-
-   cy.get('input#first_name').type('Bob')
-   cy.get('input#last_name').type('Narciso Pipoca')
-   cy.get('input#company').type('PGATS')
-
-   //Endereço
-   cy.get('input#address1').type('Avenida Selenium, nr 2004')
-   //cy.get('input#address2').type('QA address 2')
-   cy.get('select#country').select('Canada')
-   cy.get('input#state').type('California') 
-   cy.get('input#city').type('Los Angeles')
-   cy.get('[data-qa="zipcode"]').type('901001')
-   cy.get('[data-qa="mobile_number"]').type('111 222 333')
-
-   //Create account button 
-   cy.get('[data-qa="create-account"]').click()
-
-   //Tripo A - Arrange, Act, Assert
-   cy.url().should('includes','account_created')
-   cy.contains('b', 'Account Created!')
-
-   //consulta ao banco a api
-   
-})
+    // Assert
+    cy.url().should('includes', 'account_created');
+    cy.contains('b', 'Account Created!');
+  });
 
   it('Login de um usuário com e-mail e senha corretos', () => {
-    
-    cy.get('[data-qa="login-email"]').type('qa-tester-1759531270241@teste.com')
-    cy.get('[data-qa="login-password"]').type(`12345`)
-    cy.get('[data-qa="login-button"]').click()
+    cy.get('a[href="/login"]').click();
+    cy.get('[data-qa="login-email"]').type(user.email);
+    cy.get('[data-qa="login-password"]').type(user.password);
+    cy.get('[data-qa="login-button"]').click();
 
-    //Assert
+    // Assert
+    cy.get('i.fa-user').parent().should('contain', user.name);
+    cy.get('a[href="/logout"]').should('be.visible');
+    cy.get(':nth-child(10) > a').should('be.visible').and('contain.text', `Logged in as ${user.name}`);
+    cy.contains('b', user.name);
+  });
 
-    cy.get('i.fa-user').parent().should('contain', 'QA tester')
-    cy.get('a[href="/logout"]').should('be.visible')
-    cy.get(':nth-child(10) > a').should('be.visible').and('contain.text','Logged in as QA tester')
-    cy.contains('b', 'QA tester')
-  })
+  it('Login de um usuário com e-mail e senha incorretos', () => {
+    cy.get('a[href="/login"]').click();
+    cy.get('[data-qa="login-email"]').type(user.email);
+    cy.get('[data-qa="login-password"]').type('wrong-password');
+    cy.get('[data-qa="login-button"]').click();
 
-   it('Login de um usuário com e-mail e senha incorretos', () => {
-  
-    cy.get('[data-qa="login-email"]').type('qa-tester-1759531270241@teste.com')
-    cy.get('[data-qa="login-password"]').type(`123456`)
-    cy.get('[data-qa="login-button"]').click()
+    // Assert
+    cy.get('.login-form > form > p').should('contain', 'Your email or password is incorrect!');
+  });
 
-    //Assert
-    cy.get('.login-form > form > p').should('contain','Your email or password is incorrect!')
+  it('Logout do usuário', () => {
+    cy.get('a[href="/login"]').click();
+    cy.get('[data-qa="login-email"]').type(user.email);
+    cy.get('[data-qa="login-password"]').type(user.password);
+    cy.get('[data-qa="login-button"]').click();
+    cy.get('i.fa-user').parent().should('contain', user.name);
 
-   })
+    // Act
+    cy.get('a[href="/logout"]').should('be.visible').click();
 
-    it.only('Logout do usuário', () => {
+    // Assert
+    cy.url().should('includes', 'login');
+    cy.contains('Login to your account');
+    cy.get('a[href="/logout"]').should('not.exist');
+    cy.get('a[href="/login"]').should('be.visible').contains('Signup / Login');
+  });
 
-    cy.get('[data-qa="login-email"]').type('qa-tester-1759531270241@teste.com')
-    cy.get('[data-qa="login-password"]').type(`12345`)
-    cy.get('[data-qa="login-button"]').click()
+  it('Cadastrar Usuário com e-mail existente no sistema', () => {
+    cy.get('a[href="/login"]').click();
+    cy.get('[data-qa="signup-name"]').type(user.name);
+    cy.get('[data-qa="signup-email"]').type(user.email);
+    cy.contains('button', 'Signup').click();
 
-    cy.get('i.fa-user').parent().should('contain', 'QA tester')
-  
-    //Act
-    cy.get('a[href="/logout"]').should('be.visible').click()
+    // Assert
+    cy.get('.signup-form > form > p').should('contain', 'Email Address already exist!');
+  });
 
-    //Assert
-    cy.url().should('includes','login')
-    cy.contains('Login to your account')
-    cy.get('a[href="/logout"]').should('not.exist')
-    cy.get('a[href="/login"]').should('be.visible').contains('Signup / Login')
-
-   })
-
-
-    it('Cadastrar Usuário com e-mail existente no sistema', () => {
-
-    //abre o link para informar os dados do usuário já existente
-    cy.get('[data-qa="signup-name"]').type('QA tester')
-    cy.get('[data-qa="signup-email"]').type( `qa-tester-1759531270241@teste.com `)
-
-    cy.contains('button','Signup').click()
-
-    cy.get('.signup-form > form > p').should('contain','Email Address already exist!')
-
-  })
-
-   it('Contact us', () => {
-
-        cy.get('a[href*=contact]').click()
-        
-       cy.get('[data-qa="name"]').type(userData.name)
-       cy.get('[data-qa="email"]').type(userData.email)
-       cy.get('[data-qa="subject"]').type(userData.subject)
-       cy.get('[data-qa="message"]').type(userData.message)
-
-        //cy.get('[name="upload_file"]').selectFile('C:\\Users\\crisc\\ProjetoPos\\pgats-02-prog-web\\cypress\\screenshots\\specs.cy.js\\Cadastrar entradas e saídas com bugs -- Cadastrar uma nova transação de entrada - falha 1 (failed).png');
-        cy.fixture('exercicio01.json').as('arquivo')
-        cy.get('[name="upload_file"]').selectFile('@arquivo')
-       
-        cy.get('[data-qa="submit-button"]').click()
-       
-       cy.get('.status').should('be.visible')
-       cy.get('.status').should('have.text','Success! Your details have been submitted successfully.')
-    
-    })
-
-  
-})
-  
+  it('Contact us', () => {
+    cy.get('a[href*=contact]').click();
+    cy.get('[data-qa="name"]').type(userData.name);
+    cy.get('[data-qa="email"]').type(userData.email);
+    cy.get('[data-qa="subject"]').type(userData.subject);
+    cy.get('[data-qa="message"]').type(userData.message);
+    cy.fixture('exercicio01.json').as('arquivo');
+    cy.get('[name="upload_file"]').selectFile('@arquivo');
+    cy.get('[data-qa="submit-button"]').click();
+    cy.get('.status').should('be.visible').and('have.text', 'Success! Your details have been submitted successfully.');
+  });
+});
--- cypress/e2e/automation-exercise.cy.js
+++ cypress/e2e/automation-exercise.cy.js
@@ -1,174 +1,120 @@
-
-
 /// <reference types="cypress" />
 
-import userData from "../fixtures/example.json"
-import { getRandomEmail, getRandomNumber } from "../support/helpers";
+import userData from "../fixtures/example.json";
 var Chance = require('chance');
 var chance = new Chance();
 
-const { log } = require("console");
-
-
 describe('Automation Exercise', () => {
+  const user = {};
+
+  before(() => {
+    user.name = chance.name();
+    user.email = `qa-tester-${chance.integer()}@teste.com`;
+    user.password = '12345';
+    user.firstName = chance.first();
+    user.lastName = chance.last();
+    user.company = chance.company();
+    user.address = chance.address();
+    user.state = chance.state({ full: true });
+    user.city = chance.city();
+    user.zipcode = chance.zip();
+    user.mobile_number = chance.phone();
+  });
+
   beforeEach(() => {
+    cy.viewport('iphone-xr');
+    cy.visit('https://automationexercise.com/');
+  });
 
-   cy.viewport('iphone-xr')
-      
-    //abrir o site
-   cy.visit('https://automationexercise.com/')
+  it('Cadastrar um usuário', () => {
+    cy.get('a[href="/login"]').click();
+    cy.get('[data-qa="signup-name"]').type(user.name);
+    cy.get('[data-qa="signup-email"]').type(user.email);
+    cy.contains('button', 'Signup').click();
 
-    //Clicar no botão para criar um novo usuário
-   cy.get('a[href="/login"]').click()
+    // Tela de cadastro
+    cy.get('input[type=radio]').check('Mrs');
+    cy.get('input#password').type(user.password, { log: false });
+    cy.get('[data-qa=days]').select('20');
+    cy.get('[data-qa=months]').select('September');
+    cy.get('[data-qa=years]').select('1992');
+    cy.get('input[type=checkbox]#newsletter').check();
+    cy.get('input[type=checkbox]#optin').check();
+    cy.get('input#first_name').type(user.firstName);
+    cy.get('input#last_name').type(user.lastName);
+    cy.get('input#company').type(user.company);
+    cy.get('input#address1').type(user.address);
+    cy.get('select#country').select('Canada');
+    cy.get('input#state').type(user.state);
+    cy.get('input#city').type(user.city);
+    cy.get('[data-qa="zipcode"]').type(user.zipcode);
+    cy.get('[data-qa="mobile_number"]').type(user.mobile_number);
+    cy.get('[data-qa="create-account"]').click();
 
-  })
-
-  it('Log',()=> {
-    cy.log(getRandomNumber())
-    cy.log(getRandomEmail())
-    cy.log(chance.address())
-   
-  })
-
-  
-  it.only('Cadastrar um usuário', () => {
-
-    const timeStamp = new Date().getTime();
-  
-    //abre o link para informar os dados do novo usuário
-    //cy.get('[data-qa="signup-name"]').type('QA tester')
-    cy.get('[data-qa="signup-name"]').type(chance.name())
-
-    //cy.get('[data-qa="signup-email"]').type( `qa-tester-${timeStamp}@teste.com `)
-    cy.get('[data-qa="signup-email"]').type( `qa-tester-${chance.integer()}@teste.com `)
-   // cy.get('[data-qa="signup-button"]').click()
-   cy.contains('button','Signup').click()
-
-   //Tela de cadastro
-
-   //cy.get('#id_gender1').check()
-   cy.get('input[type=radio]').check('Mrs')
-   //cy.get('input[type=password]').type('12345')
-   cy.get('input#password').type('12345', {log:false})
-
-   //para combobox e selectors
-   cy.get('[data-qa=days]').select('20')
-   cy.get('[data-qa=months]').select('September')
-   cy.get('[data-qa=years]').select('1992')
-
-   //radio button e checkboxs
-   cy.get('input[type=checkbox]#newsletter').check()
-   cy.get('input[type=checkbox]#optin').check()
-
-   cy.get('input#first_name').type('Bob')
-   cy.get('input#last_name').type('Narciso Pipoca')
-   cy.get('input#company').type('PGATS')
-
-   //Endereço
-   cy.get('input#address1').type('Avenida Selenium, nr 2004')
-   //cy.get('input#address2').type('QA address 2')
-   cy.get('select#country').select('Canada')
-   cy.get('input#state').type('California') 
-   cy.get('input#city').type('Los Angeles')
-   cy.get('[data-qa="zipcode"]').type('901001')
-   cy.get('[data-qa="mobile_number"]').type('111 222 333')
-
-   //Create account button 
-   cy.get('[data-qa="create-account"]').click()
-
-   //Tripo A - Arrange, Act, Assert
-   cy.url().should('includes','account_created')
-   cy.contains('b', 'Account Created!')
-
-   //consulta ao banco a api
-   cy.log(userData.email)
-   cy.log(userData.name)   
-
-})
+    // Assert
+    cy.url().should('includes', 'account_created');
+    cy.contains('b', 'Account Created!');
+  });
 
   it('Login de um usuário com e-mail e senha corretos', () => {
-    
-    cy.get('[data-qa="login-email"]').type('qa-tester-1759531270241@teste.com')
-    cy.get('[data-qa="login-password"]').type(`12345`)
-    cy.get('[data-qa="login-button"]').click()
+    cy.get('a[href="/login"]').click();
+    cy.get('[data-qa="login-email"]').type(user.email);
+    cy.get('[data-qa="login-password"]').type(user.password);
+    cy.get('[data-qa="login-button"]').click();
 
-    //Assert
+    // Assert
+    cy.get('i.fa-user').parent().should('contain', user.name);
+    cy.get('a[href="/logout"]').should('be.visible');
+    cy.get(':nth-child(10) > a').should('be.visible').and('contain.text', `Logged in as ${user.name}`);
+    cy.contains('b', user.name);
+  });
 
-    cy.get('i.fa-user').parent().should('contain', 'QA tester')
-    cy.get('a[href="/logout"]').should('be.visible')
-    cy.get(':nth-child(10) > a').should('be.visible').and('contain.text','Logged in as QA tester')
-    cy.contains('b', 'QA tester')
-  })
+  it('Login de um usuário com e-mail e senha incorretos', () => {
+    cy.get('a[href="/login"]').click();
+    cy.get('[data-qa="login-email"]').type(user.email);
+    cy.get('[data-qa="login-password"]').type(chance.string({ length: 10 }));
+    cy.get('[data-qa="login-button"]').click();
 
-   it('Login de um usuário com e-mail e senha incorretos', () => {
-  
-    cy.get('[data-qa="login-email"]').type('qa-tester-1759531270241@teste.com')
-    cy.get('[data-qa="login-password"]').type(`123456`)
-    cy.get('[data-qa="login-button"]').click()
+    // Assert
+    cy.get('.login-form > form > p').should('contain', 'Your email or password is incorrect!');
+  });
 
-    //Assert
-    cy.get('.login-form > form > p').should('contain','Your email or password is incorrect!')
+  it('Logout do usuário', () => {
+    cy.get('a[href="/login"]').click();
+    cy.get('[data-qa="login-email"]').type(user.email);
+    cy.get('[data-qa="login-password"]').type(user.password);
+    cy.get('[data-qa="login-button"]').click();
+    cy.get('i.fa-user').parent().should('contain', user.name);
 
-   })
+    // Act
+    cy.get('a[href="/logout"]').should('be.visible').click();
 
-    it.only('Logout do usuário', () => {
+    // Assert
+    cy.url().should('includes', 'login');
+    cy.contains('Login to your account');
+    cy.get('a[href="/logout"]').should('not.exist');
+    cy.get('a[href="/login"]').should('be.visible').contains('Signup / Login');
+  });
 
-    cy.get('[data-qa="login-email"]').type('qa-tester-1759531270241@teste.com')
-    cy.get('[data-qa="login-password"]').type(`12345`)
-    cy.get('[data-qa="login-button"]').click()
+  it('Cadastrar Usuário com e-mail existente no sistema', () => {
+    cy.get('a[href="/login"]').click();
+    cy.get('[data-qa="signup-name"]').type(user.name);
+    cy.get('[data-qa="signup-email"]').type(user.email);
+    cy.contains('button', 'Signup').click();
 
-    cy.get('i.fa-user').parent().should('contain', 'QA tester')
-  
-    //Act
-    cy.get('a[href="/logout"]').should('be.visible').click()
+    // Assert
+    cy.get('.signup-form > form > p').should('contain', 'Email Address already exist!');
+  });
 
-    //Assert
-    cy.url().should('includes','login')
-    cy.contains('Login to your account')
-    cy.get('a[href="/logout"]').should('not.exist')
-    cy.get('a[href="/login"]').should('be.visible').contains('Signup / Login')
-
-   })
-
-
-    it('Cadastrar Usuário com e-mail existente no sistema', () => {
-
-    //abre o link para informar os dados do usuário já existente
-    cy.get('[data-qa="signup-name"]').type('QA tester')
-    cy.get('[data-qa="signup-email"]').type( `qa-tester-1759531270241@teste.com `)
-
-    cy.contains('button','Signup').click()
-
-    cy.get('.signup-form > form > p').should('contain','Email Address already exist!')
-
-  })
-
-   it('Contact us', () => {
-
-        cy.get('a[href*=contact]').click()
-        
-      //  cy.get('[data-qa="name"]').type(userData.name)
-      //  cy.get('[data-qa="email"]').type(userData.email)
-     //   cy.get('[data-qa="subject"]').type(userData.subject)
-     //   cy.get('[data-qa="message"]').type(userData.message)
-
-        cy.get('[data-qa="name"]').type(chance.string())
-        cy.get('[data-qa="email"]').type(userData.email)
-        cy.get('[data-qa="subject"]').type(chance.string())
-        cy.get('[data-qa="message"]').type(chance.string())
-       
-       
-        //cy.get('[name="upload_file"]').selectFile('C:\\Users\\crisc\\ProjetoPos\\pgats-02-prog-web\\cypress\\screenshots\\specs.cy.js\\Cadastrar entradas e saídas com bugs -- Cadastrar uma nova transação de entrada - falha 1 (failed).png');
-        cy.fixture('exercicio01.json').as('arquivo')
-        cy.get('[name="upload_file"]').selectFile('@arquivo')
-       
-        cy.get('[data-qa="submit-button"]').click()
-       
-       cy.get('.status').should('be.visible')
-       cy.get('.status').should('have.text','Success! Your details have been submitted successfully.')
-    
-    })
-
-  
-})
-  
+  it('Contact us', () => {
+    cy.get('a[href*=contact]').click();
+    cy.get('[data-qa="name"]').type(chance.name());
+    cy.get('[data-qa="email"]').type(chance.email());
+    cy.get('[data-qa="subject"]').type(chance.sentence({ words: 3 }));
+    cy.get('[data-qa="message"]').type(chance.paragraph());
+    cy.fixture('exercicio01.json').as('arquivo');
+    cy.get('[name="upload_file"]').selectFile('@arquivo');
+    cy.get('[data-qa="submit-button"]').click();
+    cy.get('.status').should('be.visible').and('have.text', 'Success! Your details have been submitted successfully.');
+  });
+});
